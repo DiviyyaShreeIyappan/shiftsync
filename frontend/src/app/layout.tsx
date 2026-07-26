941dca823c54d01ef5646f96c67a0831a556df77
DiviyyaShreeIyappan <diviyya8@gmail.com>
2026-07-26 17:23:33 +0100

    configured next.js with typescript for the frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+
+export const metadata: Metadata = {
+  title: "ShiftSync",
+  description: "Staff scheduling system",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>ShiftSync</h1>
+      <p>Distributed Staff Scheduling System</p>
+    </main>
+  );
+}
